6b571bb912d84d3d09b0fce706b6635c6665ec25
tyspt <jiaweitang828@gmail.com>
2020-11-17 01:36:10 +0100

    Generate preload data for demo

--- src/main/java/de/oth/regensburg/projektstudium/backend/Config.java
+++ src/main/java/de/oth/regensburg/projektstudium/backend/Config.java
@@ -6,7 +6,10 @@ import de.oth.regensburg.projektstudium.backend.entity.Employee;
 import de.oth.regensburg.projektstudium.backend.entity.Package;
 import de.oth.regensburg.projektstudium.backend.entity.enums.PackageStatus;
 import de.oth.regensburg.projektstudium.backend.entity.enums.PackageType;
-import de.oth.regensburg.projektstudium.backend.repository.*;
+import de.oth.regensburg.projektstudium.backend.repository.BuildingRepository;
+import de.oth.regensburg.projektstudium.backend.repository.DriverRepository;
+import de.oth.regensburg.projektstudium.backend.repository.EmployeeRepository;
+import de.oth.regensburg.projektstudium.backend.repository.PackageRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -20,7 +23,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Random;
 
 @Configuration
 public class Config extends WebMvcConfigurerAdapter {
@@ -37,46 +39,45 @@ public class Config extends WebMvcConfigurerAdapter {
             BuildingRepository buildingRepository,
             EmployeeRepository employeeRepository,
             PackageRepository packageRepository,
-            HandoverRepository handoverRepository,
             DriverRepository driverRepository
     ) {
         return args -> {
-            Building f36 = new Building("F36", "Forschungszentrum", "Bla bla", "Demostraße 42, Regensburg");
-            Building d47 = new Building("D47", "Spaßgebaude", "Bla bla", "Spaßstraße 39a, Regensburg");
+            Building f36 = new Building("F36", "Forschungszentrum", "Dies ist ein Demogebäude.", "Dummystraße 19, Regensburg");
+            Building u22 = new Building("H47", "Sammelgebäude", "Hintergrund: Keilberg mit der Steinbruchkante, Kirchturm von St. Anton, Zuckerfabrik.", "Universitätsstraße 31, 93053 Regensburg");
             log.info("Preloading " + buildingRepository.save(f36));
-            log.info("Preloading " + buildingRepository.save(d47));
+            log.info("Preloading " + buildingRepository.save(u22));
 
-            Employee secretary = new Employee("Lisa Sekretärin", "lisa.sekretaerin@demo.de", "333333", f36, "Demostraße 42, F36/2/8, Regensburg");
-            Employee maxMustermann = new Employee("Max Mustermann", "max.mustermann@demo.de", "11111111", f36, "Demostraße 42, F36/2/8, Regensburg");
-            Employee annamMusterfrau = new Employee("Anna Musterfrau", "anna.musterfrau@demo.de", "22222222", d47, "Spaßstraße 39a, D47/6/3, Regensburg");
-            log.info("Preloading " + employeeRepository.save(secretary));
+            Employee lisaSekretaerin = new Employee("Lisa Sekretärin", "lisa.sekretaerin@demo.de", "0134/994474454", f36, "Dummystraße 19, F36/2/8, Regensburg");
+            Employee maxMustermann = new Employee("Max Mustermann", "max.mustermann@demo.de", "0158/61664165", f36, "Dummystraße 19, F36/2/6, Regensburg");
+            Employee annamMusterfrau = new Employee("Anna Musterfrau", "anna.musterfrau@demo.de", "0187/165243152", u22, "Universitätsstraße 31, U22/6/3, Regensburg");
+            Employee tomCruise = new Employee("Tom Cruise", "tom.cruise@demo.de", "0189/164642543", u22, "Universitätsstraße 31, U22/3/1, Regensburg");
+            log.info("Preloading " + employeeRepository.save(lisaSekretaerin));
             log.info("Preloading " + employeeRepository.save(maxMustermann));
             log.info("Preloading " + employeeRepository.save(annamMusterfrau));
+            log.info("Preloading " + employeeRepository.save(tomCruise));
 
-            maxMustermann.setRepresentative(secretary);
+            maxMustermann.setRepresentative(lisaSekretaerin);
             employeeRepository.save(maxMustermann);
 
-            Random random = new Random();
+            tomCruise.setRepresentative(annamMusterfrau);
+            employeeRepository.save(tomCruise);
 
-            for (int i = 0; i < 5; i++) {
-                Package p1 = new Package(PackageType.INBOUND, Integer.toString(random.nextInt(999999999)), "SAP189271931", maxMustermann, annamMusterfrau);
-                Package p2 = new Package(PackageType.OUTBOUND, Integer.toString(random.nextInt(999999999)), "SAP9999912313", annamMusterfrau, maxMustermann);
+//            Random random = new Random();
+//            String[] inboundStatusOptions = {"CREATED"};
+//            p1.setStatus(PackageStatus.valueOf(inboundStatusOptions[random.nextInt(inboundStatusOptions.length)]));
 
-                String[] inboundStatusOptions = {"CREATED"};
-                String[] outboundStatusOptions = {"CREATED"};
+            Package p1 = new Package(PackageType.INBOUND, "4337185786433", "SAP189271931", maxMustermann, annamMusterfrau);
+            p1.setStatus(PackageStatus.CREATED);
+            log.info("Preloading " + packageRepository.save(p1));
 
-                p1.setStatus(PackageStatus.valueOf(inboundStatusOptions[random.nextInt(inboundStatusOptions.length)]));
-                p2.setStatus(PackageStatus.valueOf(outboundStatusOptions[random.nextInt(outboundStatusOptions.length)]));
-                log.info("Preloading " + packageRepository.save(p1));
-                log.info("Preloading " + packageRepository.save(p2));
-            }
+            Package p2 = new Package(PackageType.INBOUND, "73203136132591", "SAP189271931", tomCruise, lisaSekretaerin);
+            p1.setStatus(PackageStatus.CREATED);
+            log.info("Preloading " + packageRepository.save(p2));
 
-//            Handover h1 = new Handover(UUID.randomUUID());
-//            h1.addPackage(p1);
-//            h1.addPackage(p2);
-//            handoverRepository.save(h1);
-//            packageRepository.save(p1);
-//            packageRepository.save(p2);
+//            Package p2 = new Package(PackageType.OUTBOUND, Integer.toString(random.nextInt(999999999)), "SAP9999912313", annamMusterfrau, maxMustermann);
+//            String[] outboundStatusOptions = {"CREATED"};
+//            p2.setStatus(PackageStatus.valueOf(outboundStatusOptions[random.nextInt(outboundStatusOptions.length)]));
+//            log.info("Preloading " + packageRepository.save(p2));
 
             Driver d1 = new Driver("Demo Driver 1", "driver1@demo.de", "d123456789", "Best Logistics Ltd.");
             log.info("Preloading " + driverRepository.save(d1));
